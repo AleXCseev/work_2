3beef9a3f75075be9bfc39d31e4250c3bbdff936
Alekseev Konstantin <alexcseev1990@gmail.com>
2020-07-12 15:17:42 +0300

    add sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,16 @@
 import modals from "./modules/modals";
+import slider from "./modules/sliders";
+import forms from "./modules/forms";
 
 window.addEventListener("DOMContentLoaded", () => {
   modals();
+  slider(
+    ".feedback-slider-item",
+    "horizontal",
+    ".main-prev-btn",
+    ".main-next-btn"
+  );
+  slider(".main-slider-item", "vertical");
+
+  forms();
 });
