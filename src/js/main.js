d57c5605339b1625d3d176017ccba0673b664b26
Alekseev Konstantin <alexcseev1990@gmail.com>
2020-07-18 14:33:53 +0300

    add mask

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 import modals from "./modules/modals";
 import slider from "./modules/sliders";
 import forms from "./modules/forms";
+import mask from "./modules/mask";
+import checkTextInputs from "./modules/checkTextInputs";
+import showMoreStyles from "./modules/showMoreStyles";
 
 window.addEventListener("DOMContentLoaded", () => {
   modals();
@@ -11,6 +14,9 @@ window.addEventListener("DOMContentLoaded", () => {
     ".main-next-btn"
   );
   slider(".main-slider-item", "vertical");
-
   forms();
+  mask('[name="phone"]');
+  checkTextInputs('[name="name"]');
+  checkTextInputs('[name="message"]');
+  showMoreStyles(".button-styles", ".styles-2");
 });
